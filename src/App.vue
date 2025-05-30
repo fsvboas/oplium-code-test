9b0916b5c2af8a6827f4f12248dc40c44fd61eb9
fsvboas <fsvboas.dev@gmail.com>
2025-05-29 22:26:04 -0300

    feat: create TodoTaskCard component initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import TodoInput from './components/TodoInput.vue'
+import TodoTaskCard from './components/TodoTaskCard.vue'
 </script>
+
 <template>
-  <div class="min-h-screen bg-neutral-50 flex justify-center items-center">
+  <div class="min-h-screen bg-neutral-50 flex justify-center items-center p-2">
     <div
-      class="flex items-center space-x-2 bg-neutral-200 max-w-3xl w-full justify-center p-4 md:p-12 rounded-md border"
+      class="flex flex-col space-y-2 items-center bg-neutral-200 max-w-3xl w-full justify-center p-4 md:p-12 rounded border"
     >
       <TodoInput />
+      <TodoTaskCard />
     </div>
   </div>
 </template>
---
+++ src/components/TodoTaskCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { NCheckbox, NButton, NIcon } from 'naive-ui'
+import { Trash } from 'lucide-vue-next'
+import { ref } from 'vue'
+
+const isTaskCompleted = ref<boolean>(false)
+</script>
+
+<template>
+  <div
+    :class="[
+      'flex justify-between items-center w-full p-2 rounded',
+      isTaskCompleted ? 'bg-neutral-100' : ' bg-white',
+    ]"
+  >
+    <div class="flex items-center space-x-2">
+      <n-checkbox v-model:checked="isTaskCompleted" />
+
+      <p
+        :class="[
+          'transition-colors duration-200',
+          isTaskCompleted ? 'line-through text-neutral-400' : 'text-black',
+        ]"
+      >
+        Tarefa 1
+      </p>
+    </div>
+
+    <n-button quaternary type="error" class="hover:!bg-red-100">
+      <template #icon>
+        <n-icon>
+          <Trash />
+        </n-icon>
+      </template>
+    </n-button>
+  </div>
+</template>
