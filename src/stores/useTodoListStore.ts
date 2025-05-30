995fd892d204c54bf4ac592364af3c0dec3ea236
fsvboas <fsvboas.dev@gmail.com>
2025-05-30 16:33:34 -0300

    feat: add localStorage persistence

--- src/stores/useTodoListStore.ts
+++ src/stores/useTodoListStore.ts
@@ -1,14 +1,23 @@
 import type { TaskType } from '@/types/taskType'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 export const useTodoListStore = defineStore('todoList', () => {
-  const todoList = ref<TaskType[]>([])
+  const storedTasks = localStorage.getItem('todoList')
+  const todoList = ref<TaskType[]>(storedTasks ? JSON.parse(storedTasks) : [])
   const id = ref(0)
 
   const addTask = (name: string) => {
     todoList.value.push({ name, id: id.value++, status: 'pending' })
   }
+
+  watch(
+    todoList,
+    (newTasks) => {
+      localStorage.setItem('todoList', JSON.stringify(newTasks))
+    },
+    { deep: true },
+  )
 
   return {
     todoList,
